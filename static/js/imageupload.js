aa5e8a34fff06eb5bdef1e6f43fb13ff034eec89
Shadab Raza <98049489+Pro1609@users.noreply.github.com>
2025-06-19 10:39:32 +0530

    Update imageupload.js

--- static/js/imageupload.js
+++ static/js/imageupload.js
@@ -1,13 +1,23 @@
 window.onload = function () {
-  const uploadBox = document.querySelectorAll('.upload-box');
+  const uploadBoxes = document.querySelectorAll('.upload-box');
+  const fileInputs = document.querySelectorAll('input[type="file"]');
 
-  uploadBox.forEach(box => {
-    box.addEventListener('mouseenter', () => {
-      box.classList.add('hovered');
-    });
+  uploadBoxes.forEach(box => {
+    box.addEventListener('mouseenter', () => box.classList.add('hovered'));
+    box.addEventListener('mouseleave', () => box.classList.remove('hovered'));
+  });
 
-    box.addEventListener('mouseleave', () => {
-      box.classList.remove('hovered');
+  fileInputs.forEach(input => {
+    input.addEventListener('change', function () {
+      if (this.files.length > 0) {
+        const parentBox = this.closest('.upload-box');
+        parentBox.classList.add('selected');
+
+        // Remove animation class after it completes
+        setTimeout(() => {
+          parentBox.classList.remove('selected');
+        }, 400);
+      }
     });
   });
 };
