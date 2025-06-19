a8baf1bec5845566d0e13aa1f654072f921927d0
Shadab Raza <98049489+Pro1609@users.noreply.github.com>
2025-06-19 13:47:57 +0530

    Update report.js

--- static/js/report.js
+++ static/js/report.js
@@ -1,9 +1,10 @@
+// report.js
 window.onload = function () {
   const boxes = document.querySelectorAll('.report-box');
 
   boxes.forEach((box, index) => {
     setTimeout(() => {
       box.classList.add('slide-in');
-    }, index * 200); // slight delay for staggered animation
+    }, index * 300); // staggered delay for each box
   });
 };
