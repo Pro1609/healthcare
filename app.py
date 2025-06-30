59eba3fbc6f06c5e9ac78aa53588bda05b537528
Shadab Raza <98049489+Pro1609@users.noreply.github.com>
2025-06-30 23:59:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -482,6 +482,9 @@ def consult():
         print("❌ Consult route error:", str(e))
         return jsonify({"results": []})
 
+@app.route('/thankyou')
+def thankyou():
+    return render_template('thankyou.html')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=10000)
